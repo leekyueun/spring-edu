48e8acacb8bb431622a284f280bd9f0f07c79acf
leekyueun <67741402+leekyueun@users.noreply.github.com>
2020-12-15 13:53:00 +0900

    add: product sale

---
+++ src/main/java/com/kyueun/apis/route/SaleRoute.java
@@ -0,0 +1,28 @@
+package com.kyueun.apis.route;
+
+import com.kyueun.apis.model.Sale;
+import com.kyueun.apis.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sale")
+public class SaleRoute {
+    private final SaleService saleService;
+
+    @Autowired
+    public SaleRoute(SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    @GetMapping("/{sale_id}")
+    @ResponseBody
+    public Sale getSale(@PathVariable(value = "sale_id") String saleId) throws Exception {
+        return this.saleService.find(Integer.parseInt(saleId));
+    }
+
+    @GetMapping("/initialize")
+    public void initializeSales() {
+        this.saleService.initializeSales();
+    }
+}
