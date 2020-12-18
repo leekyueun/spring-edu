202260ff823b72c4817d6dde35b7500dca1c3e1b
leekyueun <67741402+leekyueun@users.noreply.github.com>
2020-12-18 09:56:04 +0900

    feat: add InitializeRoute

---
+++ src/main/java/com/kyueun/apis/route/InitializeRoute.java
@@ -0,0 +1,35 @@
+package com.kyueun.apis.route;
+
+import com.kyueun.apis.service.ProductService;
+import com.kyueun.apis.service.ReviewService;
+import com.kyueun.apis.service.SaleService;
+import com.kyueun.apis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/initialize")
+public class InitializeRoute {
+    private final ProductService productService;
+    private final ReviewService reviewService;
+    private final SaleService saleService;
+    private final UserService userService;
+
+    @Autowired
+    public InitializeRoute(ProductService productService, ReviewService reviewService, SaleService saleService, UserService userService) {
+        this.productService = productService;
+        this.reviewService = reviewService;
+        this.saleService = saleService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public void initialize() {
+        this.productService.initializeProducts();
+        this.userService.initializeUsers();
+        this.reviewService.initializeReviews();
+        this.saleService.initializeSales();
+    }
+}
